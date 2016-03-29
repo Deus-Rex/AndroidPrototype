6d7fe68ec2c6fd1f5ffdc64654b705b6ca9a5eb4
Sharp <ryansharp1995@gmail.com>
2016-03-29 02:45:41 +0100

    Added other heart animations for different traffic levels

--- app/src/main/java/com/example/trafficscotlandproto/Fragments/AmbientModeFragment.java
+++ app/src/main/java/com/example/trafficscotlandproto/Fragments/AmbientModeFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.trafficscotlandproto.MainActivity;
 import com.example.trafficscotlandproto.R;
@@ -23,25 +24,41 @@ public class AmbientModeFragment extends DialogFragment {
         // Get views
         View view = inflater.inflate(R.layout.fragment_ambient_mode, null);
         ImageView imageView = (ImageView) view.findViewById(R.id.imgHeartbeat);
+        TextView tvOutput = (TextView) view.findViewById(R.id.txtOutput);
 ;
         // Get number of traffic reports
-        int noOfPlanned = MainActivity.rssPlanned.getLength();
         int noOfRoadworks = MainActivity.rssRoadworks.getLength();
         int noOfIncidents = MainActivity.rssIncidents.getLength();
-        int totalTraffic = noOfIncidents + noOfPlanned + noOfRoadworks;
+        int totalTraffic = noOfIncidents + noOfRoadworks;
 
         // Show a different colour of heart depending on no of reports
         int resource;
-        if (totalTraffic > 300) resource = R.drawable.heart_rage;
-        else if (totalTraffic > 150) resource = R.drawable.heart_stress;
-        else if (totalTraffic > 50) resource = R.drawable.heart_mild;
-        else resource = R.drawable.heart_calm;
+        int animation = R.anim.pulse_heart;
+        String output = Integer.toString(totalTraffic) + " total reports\n";
 
-//        R.anim.pulse_heart
+        if (totalTraffic > 300) {
+            resource = R.drawable.heart_rage;
+            animation = R.anim.pulse_rage;
+            output += getString(R.string.msgRage);
+        } else if (totalTraffic > 150) {
+            resource = R.drawable.heart_stress;
+            animation = R.anim.pulse_stress;
+            output += getString(R.string.msgStress);
+        } else if (totalTraffic > 50) {
+            resource = R.drawable.heart_mild;
+            animation = R.anim.pulse_mild;
+            output += getString(R.string.msgMild);
+        } else {
+            resource = R.drawable.heart_calm;
+            animation = R.anim.pulse_calm;
+            output += getString(R.string.msgCalm);
+        }
 
         // Set resource and start animation
         imageView.setBackgroundResource(resource);
-        imageView.startAnimation(AnimationUtils.loadAnimation(getActivity(), R.anim.pulse_heart));
+        imageView.startAnimation(AnimationUtils.loadAnimation(getActivity(), animation));
+
+        tvOutput.setText(output);
 
         // Add items to view and create dialog
         builder.setView(view);
